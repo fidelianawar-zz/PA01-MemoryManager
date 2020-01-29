edfb479d785c8cf453ee4761776d439ecc66fad3
Fidelia Nawar <fidelianawar@Fidelias-MacBook-Pro-2.local>
2020-01-29 16:26:14 -0600

    initial test

---
+++ cmake-build-debug/CMakeFiles/MemoryManager.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MemoryManager.dir/CustomMemoryManager.cpp.o"
+  "CMakeFiles/MemoryManager.dir/algoNew.cpp.o"
+  "CMakeFiles/MemoryManager.dir/main.cpp.o"
+  "MemoryManager"
+  "MemoryManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MemoryManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
